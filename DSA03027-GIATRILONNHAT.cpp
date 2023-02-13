f9b6f2cfc7c4b75dc7b2734e96d46fd02fd0e633
quochuy148 <wuochuy02@gmail.com>
2023-02-13 21:03:13 +0700

    DSA

---
+++ DSA03027-GIATRILONNHAT.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define ms(s,n) memset(s,n,sizeof(s))
+#define all(a) a.begin(),a.end()
+#define sz(a) int((a).size())
+#define f0(i,n) for (int i=0; i<n; i++)
+#define f1(i,n) for (int i=1; i<=n; i++)
+#define FastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define TC() int t; cin >> t; while (t--)
+#define el cout << "\n"
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define maxn 
+
+const int MOD = (int) 1e9+7;
+
+void FileIO(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main(){
+    FileIO();
+    FastIO;
+    int n; cin >> n;
+    vi a(n);
+    f0 (i,n) cin >> a[i];
+    ll sum = 0;
+    f0 (i,n){
+    	if (a[i] > 0) sum += 2 * abs(a[i]);
+    }
+    cout << sum;
+    return 0;
+}
