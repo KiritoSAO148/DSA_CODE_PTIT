d1d114e579467d67850e2f1ed17b083476208d25
quochuy148 <wuochuy02@gmail.com>
2023-02-28 18:47:35 +0700

    DSA

--- DSA10011-DICHUYENTRENBANGSO.cpp
+++ DSA10011-DICHUYENTRENBANGSO.cpp
@@ -1,77 +1,100 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
 #define ms(s,n) memset(s,n,sizeof(s))
 #define all(a) a.begin(),a.end()
 #define sz(a) int((a).size())
+#define f0(i,n) for (int i=0; i<n; i++)
+#define f1(i,n) for (int i=1; i<=n; i++)
+#define FastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define TC() int t; cin >> t; while (t--)
+#define el cout << "\n"
 #define pb push_back
 #define pf push_front
 #define fi first
 #define se second
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int,int> pi;
-typedef vector<int> vi;
-typedef vector<pi> vii;
+#define maxn 
 
 const int MOD = (int) 1e9+7;
 
-int n, m;
-int a[505][505];
-bool visited[501][501];
-
-int dx[4]={0,-1,0,1};
-int dy[4]={-1,0,1,0};
-
-struct cmp{
-    bool operator () (pair<int, pair<int, int>> a, pair<int, pair<int, int>> b){
-        return a.se.se > b.se.se;
-    }
-};
-
-void inp(){
-    ms(visited,false);
-    cin >> n >> m;
-    for (int i=1; i<=n; i++){
-        for (int j=1; j<=m; j++) cin >> a[i][j];
-    }
-}
-
-int bfs(){
-    priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,cmp>pq;
-    pq.push({1,{1,a[1][1]}});
-    visited[1][1]=true;
-    while(!pq.empty()){
-        pair<int,pair<int,int>>top=pq.top(); pq.pop();
-        int i=top.fi;
-        int j=top.se.fi;
-        if (i==n && j==m) return top.se.se;
-        for(int k = 0; k < 4; k++){
-            int i1 = i + dx[k];
-            int j1 = j + dy[k];
-            // if(i == n && j == m)
-            //     return top.se.se + a[i][j];
-            if(i1 >= 1 && i1 <= n && j1 >= 1 && j1 <= m && !visited[i1][j1]){
-                visited[i1][j1] = true;
-                pq.push({i1, {j1, top.se.se + a[i1][j1]}});
-            }
-        }
-    }
-}
-
-int main(){
+void FileIO(){
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     #endif
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    int t; cin >> t;
-    while (t--){
-        inp();
-        cout << bfs() << "\n";
+}
+
+int n, m, a[500][500], dp[500][500];
+
+int dx[4]={1,0,0,-1};
+int dy[4]={0,-1,1,0};
+
+struct cell{
+	int x,y;
+	int dis;
+	cell (int x, int y, int dis)
+	: x(x)
+	, y(y)
+	, dis(dis)
+	{
+	}
+};
+
+bool operator<(const cell& a, const cell& b){
+    if (a.dis == b.dis) {
+        if (a.x != b.x) return (a.x < b.x);
+        return (a.y < b.y);
+    }
+    return (a.dis < b.dis);
+}
+
+bool check(int i, int j){
+    return (i >= 0 && i < n && j >= 0 && j < m);
+}
+
+int solve(){
+    f0 (i,n) f0 (j,m) dp[i][j] = INT_MAX;
+    set<cell> st;
+    st.insert(cell(0, 0, 0));
+    dp[0][0] = a[0][0];
+    while (!st.empty()) {
+        cell k = *st.begin();
+        st.erase(st.begin());
+        for (int i = 0; i < 4; ++i) {
+            int x = k.x + dx[i];
+            int y = k.y + dy[i];
+            if (!check(x, y))
+                continue;
+            if (dp[x][y] > dp[k.x][k.y] + a[x][y]) {
+                if (dp[x][y] != INT_MAX)
+                    st.erase(st.find(cell(x, y, dp[x][y])));
+                dp[x][y] = dp[k.x][k.y] + a[x][y];
+                st.insert(cell(x, y, dp[x][y]));
+            }
+        }
+    }
+    return dp[n - 1][m - 1];
+}
+
+int main(){
+    FileIO();
+    FastIO;
+    TC(){
+    	cin >> n >> m;
+    	ms(dp,0);
+    	f0 (i,n) f0 (j,m) cin >> a[i][j];
+    	cout << solve(); el;
     }
     return 0;
 }
