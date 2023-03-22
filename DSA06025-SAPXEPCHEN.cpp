4833ca064ee56f43c289c105069b04f9cbd7fc35
quochuy148 <wuochuy02@gmail.com>
2023-03-22 21:40:15 +0700

    Update DSA06025-SAPXEPCHEN.cpp

--- DSA06025-SAPXEPCHEN.cpp
+++ DSA06025-SAPXEPCHEN.cpp
@@ -20,7 +20,7 @@ int main(){
 	int n; cin >> n;
 	int a[n];
 	for (int &x : a) cin >> x;
-	for (int i=1; i<n; i++){
+	for (int i=0; i<n; i++){
 		int x=a[i], pos=i-1;
 		while (pos>=0 && a[pos]>x){
 			a[pos+1]=a[pos];
